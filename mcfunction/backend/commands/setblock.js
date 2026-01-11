6b5f92edf255c0188c0e980dee21081f5a5fb0a5
toast <244019107+xyztoast@users.noreply.github.com>
2026-01-11 09:16:54 -0800

    Update setblock.js

--- mcfunction/backend/commands/setblock.js
+++ mcfunction/backend/commands/setblock.js
@@ -1,1 +1,13 @@
+import { ArgTypes } from "../commandSchema.js"
 
+export default {
+  name: "setblock",
+  description: "Sets a block at a specified position",
+  syntax: "setblock <x> <y> <z> <block>",
+  args: [
+    { name: "x", type: "coordinate" },
+    { name: "y", type: "coordinate" },
+    { name: "z", type: "coordinate" },
+    { name: "block", type: "block" }
+  ]
+}
