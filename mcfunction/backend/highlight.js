9188471eb3e6cfd312e8de2a66dafa3cea2480a9
toast <244019107+xyztoast@users.noreply.github.com>
2026-03-24 16:37:48 -0700

    Update and rename scripts.js to highlight.js

---
+++ mcfunction/backend/highlight.js
@@ -0,0 +1,38 @@
+<script>
+hljs.registerLanguage("mcfunction", function(hljs) {
+  return {
+    name: "mcfunction",
+
+    keywords: {
+      keyword:
+        "say tp give clear kill effect execute summon setblock fill clone teleport time weather scoreboard function tag gamemode difficulty gamerule title tellraw playsound particle"
+    },
+
+    contains: [
+
+      {
+        className: "comment",
+        begin: /#/,
+        end: /$/
+      },
+
+      {
+        className: "string",
+        begin: /"/,
+        end: /"/
+      },
+
+      {
+        className: "selector",
+        begin: /@[pare]\b/
+      },
+
+      {
+        className: "number",
+        begin: /(~-?\d*\.?\d+|\^-?\d*\.?\d+)/
+      }
+
+    ]
+  };
+});
+</script>
