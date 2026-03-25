ede7116da98fae7625b5bd9fb343306703eceda8
idktoast2 <toastdesignshop@gmail.com>
2026-03-25 16:12:10 -0700

    Update highlight.js

--- mcfunction/backend/highlight.js
+++ mcfunction/backend/highlight.js
@@ -3,7 +3,7 @@ contains: [
   // commands only at start of line
   {
     className: "keyword",
-    begin: /^(give|tp|say|execute|summon|fill|clone|setblock|function|scoreboard|title|tellraw|playsound|particle)\b/
+    begin: 'give tp say execute summon fill clone setblock function scoreboard title tellraw playsound particle'
   },
 
   // strings
