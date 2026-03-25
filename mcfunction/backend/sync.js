8297b36caf911a3ee274740d4848252c36f40362
toast <244019107+xyztoast@users.noreply.github.com>
2026-03-24 17:06:14 -0700

    Update sync.js

--- mcfunction/backend/sync.js
+++ mcfunction/backend/sync.js
@@ -4,7 +4,7 @@ const highlightLayer = document.querySelector(".highlight-layer");
 
 function updateHighlight() {
 
-  let code = textarea.value.replace(/^\n+/, "");
+  highlighted.innerHTML = highlighted.innerHTML.replace(/^\n+/, "")
 
   // escape html
   code = code
