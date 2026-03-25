3f0b483dc39ae5678a57fedc80fc685dd00a3e1b
idktoast2 <toastdesignshop@gmail.com>
2026-03-25 16:15:11 -0700

    Update highlight.js

--- mcfunction/backend/highlight.js
+++ mcfunction/backend/highlight.js
@@ -1,11 +1,31 @@
+hljs.registerLanguage("mcfunction", function(hljs) {
+  return {
+    name: "mcfunction",
 contains: [
 
+    keywords: {
+      keyword:
+        "say tp give clear kill effect execute summon setblock fill clone teleport time weather scoreboard function tag gamemode difficulty gamerule title tellraw playsound particle"
+    },
   // commands only at start of line
   {
     className: "keyword",
-    begin: 'give tp say execute summon fill clone setblock function scoreboard title tellraw playsound particle'
+    begin: /^(give|tp|say|execute|summon|fill|clone|setblock|function|scoreboard|title|tellraw|playsound|particle)\b/
   },
 
+    contains: [
+  // comments
+  {
+    className: "comment",
+    begin: /#/,
+    end: /$/
+  },
+
+      {
+        className: "comment",
+        begin: /#/,
+        end: /$/
+      },
   // strings
   {
     className: "string",
@@ -13,16 +33,33 @@ contains: [
     end: /"/
   },
 
+      {
+        className: "string",
+        begin: /"/,
+        end: /"/
+      },
   // selectors
   {
     className: "selector",
     begin: /@[pare]\b/
   },
 
+      {
+        className: "selector",
+        begin: /@[pare]\b/
+      },
   // coordinates
   {
     className: "number",
     begin: /(~-?\d*\.?\d+|\^-?\d*\.?\d+)/
   }
 
+      {
+        className: "number",
+        begin: /(~-?\\d*\\.?\\d+|\\^-?\\d*\\.?\\d+)/
+      }
+
+    ]
+  };
+});
 ]
